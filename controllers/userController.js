ba4bda288df8d6856565f97772ab2d6e377ec78d
Mike <awaemekamichael@gmail.com>
2024-10-30 17:02:17 +0100

    added profile update

--- controllers/userController.js
+++ controllers/userController.js
@@ -125,8 +125,23 @@ const followUnfollowUser = async (req, res) =>{
 }
 
 const updateUser = async (req, res) =>{
+  const {name, email, username, profilePic, bio} = req.body
+  const userId = req.user._id;
      try{
-        const {name, email, username, password} = req.body
+       let user = await User.findById(userId);
+       if (!user) return res.status(400).json({message: "User not found"})
+      //  if(password){
+      //   const salt = await bcrypt.genSalt(10);
+      //   const hashedPassword = await bcrypt.hash(password, salt);
+      //   user.password = hashedPassword;
+      //  }
+      user.name = name || user.name;
+      user.username = username || user.username;
+      user.profilePic = profilePic || user.profilePic;
+      user.bio = bio || user.bio
+
+      user = await user.save();
+      res.status(200).json({message:"Profile has been updated succesfully", user})
      }
      catch (error){
          res.status(500).json({message: error.message})
